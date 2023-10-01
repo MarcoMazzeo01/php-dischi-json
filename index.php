292a2cdf4a19e93c2f1c7a6a9a7eb1ed2366c38e
plastikSun <marcom2k01@gmail.com>
2023-10-01 12:02:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Vue js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- script -->
+    <script src="./main.js" defer></script>
+
+    <title>Spotify Knockoff</title>
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
