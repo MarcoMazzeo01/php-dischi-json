ba9d0ea12cf983bc35fcc61ae9c1b9cff6cba211
plastikSun <marcom2k01@gmail.com>
2023-10-01 16:37:45 +0200

    install: bootstrap cdn

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+  <!-- Vue js -->
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
   <!-- Axios -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
@@ -16,12 +22,10 @@
 
 <body>
   <div id="app">
+
+
   </div>
 
-
-
-  <!-- Vue js -->
-  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
 </body>
 
